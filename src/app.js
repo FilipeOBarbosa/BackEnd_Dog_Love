9ea5ca1f7558c32e70d70806ce0d5971d3d5f853
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-10-25 21:03:26 -0300

    adicionando primeira rota

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = new express();
 
+const userRout = require('./routes/userRoute')
+
 app.use(express.json());
+
+
+
+app.use('/user',userRout);
+
 
 module.exports=app;
