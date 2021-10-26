7a715fed80077aabb6a5324cb52b3d0337ee137a
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-10-25 21:29:54 -0300

    criando o getUsers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,9 +5,17 @@ exports.post = async (request, response) =>{
     const result = await repository.create(request.body)
 
     if(result === true){
+
         return response.status(201).json({ message: "User added succesfully!" });
     }else{
-        return response.status(400).json({ error: err.message }); 
+
+        return response.status(400).json({ message: "email já cadastrado"}); 
     }
+
+}
+
+exports.get = async (request, response) =>{
+    const result = await repository.getUsers();
+    return response.status(201).json(result);
 
 }
