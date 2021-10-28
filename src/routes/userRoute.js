365d58d2347e451cf4a5f3c933b1e0d553e0c753
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-10-27 22:16:42 -0300

    nova Rota

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/UserController')
 
 router.post('/',controller.post);
 router.get('/',controller.get);
+router.delete('/',controller.delete);
 
 module.exports = router;
