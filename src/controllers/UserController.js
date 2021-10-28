c4bc269bcafb21cd8e4c75723fac1406016f5b12
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-10-27 22:15:39 -0300

    rota delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,7 +6,7 @@ exports.post = async (request, response) =>{
 
     if(result === true){
 
-        return response.status(201).json({ message: "User added succesfully!" });
+        return response.status(201).json({ message: "User adicionado com sucesso" });
     }else{
 
         return response.status(400).json({ message: "email já cadastrado"}); 
@@ -15,7 +15,19 @@ exports.post = async (request, response) =>{
 }
 
 exports.get = async (request, response) =>{
-    const result = await repository.getUsers();
+    const result = await repository.read();
     return response.status(201).json(result);
 
+}
+
+exports.delete = async (request, response)=>{
+    const {id} = request.query;
+    
+    const result = await repository.delete(id);
+
+    if(result){
+        return response.status(201).json({message: "usuário foi excluído com sucesso"})
+    }else{
+        return response.status(400).json({message: "usuário não foi excluído com sucesso"})
+    }
 }
