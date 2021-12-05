431cde5378566ad3e57e436a9fa98e231a681497
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-12-05 16:03:32 -0300

    mudança na mensagem

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,14 +1,17 @@
 const tokenService = require('./TokenService');
+const log = require('./LogService');
 
 exports.authorize = async (request, response, next) =>{
     const token = request.headers['auth']
 
     const isValid = tokenService.validateToken(token);
     if(isValid){
+        log('AuthService/authorize','Passou', true)
         next();
     }else{
+        log('AuthService/authorize','Algo deu errado aqui', false)
         response.status(401).json({
-            message: 'Token expirado'
+            message: 'Token inválido'
         });
     }
        
