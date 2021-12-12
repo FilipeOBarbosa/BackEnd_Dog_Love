03eefc4505e4517a46264f0fa1a493d70ff9ea8d
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-12-12 15:31:13 -0300

    deletando img

--- src/controllers/DogController.js
+++ src/controllers/DogController.js
@@ -2,6 +2,23 @@ const repository = require('../repositories/DogRepository');
 
 
 class DogController{
+
+    deleteImg(request, response){
+        const {name}= request.body;
+        if(!name){
+            return response.status(400).json({ message: "Informe o nome da img"}); 
+        }
+        const fs = require('fs');
+        const path = require('path')
+        const dir = path.resolve(__dirname,'..','..','public','dogs');
+        fs.unlink(dir+`/${name}`,(err)=>{
+            if (err) {
+                return response.status(404).json({ message: "img não encontrada"}); 
+            } else {
+                return response.status(200).json({ message: "deletado"});                             
+            }
+        })
+    }
     async post(request, response) {
         const fullUrl = request.protocol + '://' + request.get('Host');
         const dog ={
