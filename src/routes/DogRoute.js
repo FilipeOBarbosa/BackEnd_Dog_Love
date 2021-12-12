3d07567bea7286f911b0b7f50b1f480eda30b1fb
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-12-12 14:57:40 -0300

    Voltando versão estável

--- src/routes/DogRoute.js
+++ src/routes/DogRoute.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/DogController')
 const authService = require('../services/AuthService')
-const crypto = require('crypto')
 const multer = require('multer');
 const storage = require('../../config/MulterConfig')
 const upload = multer({storage})
 class DogRoute{
     constructor(){
-        router.delete('/delete-img',authService.authorize,controller.deleteImg)
         router.post('/',authService.authorize,upload.single('foto'),controller.post)
         router.get('/get-by-dono',authService.authorize, controller.getDogByDono)
         router.get('/',authService.authorize, controller.get);
