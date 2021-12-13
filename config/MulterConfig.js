0b12e5663c2bd95ee0248f2d1805c75e3ccb6f1a
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-12-13 10:58:04 -0300

    tirando o print

--- config/MulterConfig.js
+++ config/MulterConfig.js
@@ -1,7 +1,6 @@
 const multer = require('multer');
 const path = require('path');
 const caminhoPasta = path.resolve(__dirname,'..','public/dogs/')
-console.log(caminhoPasta)
 const update = multer.diskStorage({
     destination:  (req, file, cb) =>{
         cb(null, caminhoPasta)
